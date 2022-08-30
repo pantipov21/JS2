85c7a213dbd4ae4b79ff1642abe7e97542f8887a
Pavel Antipov <pavel@localhost>
2022-08-30 18:05:58 +0300

    Функция реализована

--- task1.js
+++ task1.js
@@ -1,17 +1,8 @@
-let quantity = Number(process.argv[2]);
-if (isNaN(quantity)==true)
-	console.log("в параметре не число");
-	
-if (quantity <=0)
-	console.log("количество должно быть строго больше нуля");
-	
-if (quantity > 0){
-	console.log("поиск %d простых чисел...", quantity);
-
+function getSimpleNumbers(qty){
+	res = [];
 	let count = 0;
 	
 	let t = 2;
-	console.log("найденные простые числа:");
 	while (count < quantity){
 		let flag = true;
 		let i = 2;
@@ -22,8 +13,24 @@ if (quantity > 0){
 		}
 		if (flag == true){
 			count++;
-			console.log("%d", t);
+			res.push(t);
 		}
 		t++;
 	}
+	return res;
+}
+
+let quantity = Number(process.argv[2]);
+if (isNaN(quantity)==true)
+	console.log("в параметре не число");
+	
+if (quantity <=0)
+	console.log("количество должно быть строго больше нуля");
+	
+if (quantity > 0){
+	console.log("поиск %d простых чисел...", quantity);
+	console.log("найденные простые числа:");
+	result = getSimpleNumbers(quantity);
+	for (let i = 0; i < quantity;i++)
+		console.log(result[i]);
 }
